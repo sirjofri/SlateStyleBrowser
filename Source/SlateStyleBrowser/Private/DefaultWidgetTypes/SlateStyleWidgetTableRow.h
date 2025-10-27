4d01df3497fd676015864c44259b8d9b3a24a8eb
sirjofri <sirjofri@sirjofri.de>
2025-10-27 16:58:43 +0100

    better visuals for table row style

--- Source/SlateStyleBrowser/Private/DefaultWidgetTypes/SlateStyleWidgetTableRow.h
+++ Source/SlateStyleBrowser/Private/DefaultWidgetTypes/SlateStyleWidgetTableRow.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "CoreMinimal.h"
+#include "Editor.h"
 #include "SlateStyleData.h"
 #include "Widgets/SlateStyleTableRow.h"
 #include "Widgets/Views/SListView.h"
@@ -15,14 +16,17 @@ public:
 		
 		DemoValues.Empty(1);
 		DemoValues.Add(MakeShared<FString>("Hello World"));
+		DemoValues.Add(MakeShared<FString>("Hello Unreal"));
+		DemoValues.Add(MakeShared<FString>("Hello Slate"));
 	
 		return SNew(SBox)
 			.VAlign(VAlign_Center)
-			.HAlign(HAlign_Left)
+			.HAlign(HAlign_Fill)
 			[
-				SNew(SListView<TSharedPtr<FString>>)
+				SAssignNew(ListView, SListView<TSharedPtr<FString>>)
 				.ListItemsSource(&DemoValues)
 				.OnGenerateRow(this, &FSlateStyleWidgetTableRow::GenerateRowWidget)
+				.OnSelectionChanged(this, &FSlateStyleWidgetTableRow::DeselectTimeout)
 			];
 	};
 
@@ -41,12 +45,29 @@ private:
 	{
 		return SNew(STableRow<TSharedPtr<FString>>, OwnerTable)
 			.Style(GetWidgetStyle<FTableRowStyle>())
+			.Padding(FMargin(20, 5))
 			[
 				SNew(STextBlock)
 				.Text(FText::FromString(*InItem))
 			];
 	}
 	
+	void DeselectAll()
+	{
+		if (ListView.IsValid())
+			ListView->ClearSelection();
+	}
+	
+	void DeselectTimeout(TSharedPtr<FString> String, ESelectInfo::Type Arg)
+	{
+		if (DeselectTimer.IsValid()) {
+			GEditor->GetTimerManager()->ClearTimer(DeselectTimer);
+		}
+		GEditor->GetTimerManager()->SetTimer(DeselectTimer, FTimerDelegate::CreateSP(this, &FSlateStyleWidgetTableRow::DeselectAll), 2., false);
+	}
+	
 private:
+	FTimerHandle DeselectTimer;
+	TSharedPtr<SListView<TSharedPtr<FString>>> ListView;
 	TArray<TSharedPtr<FString>> DemoValues;
 };
